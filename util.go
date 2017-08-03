bc80aa761b15e8d6cb95388dd826550c0b4e7e5d
crosalot <crosalot#gmail.com>
2017-08-03 16:07:57 +0700

    endline

--- util.go
+++ util.go
@@ -19,14 +19,14 @@ func Getenv(key, fallback string) string {
 func Elog(num int64, msg string, err error, domain string, oid string, mid string, email string) {
 	msg = strings.Replace(msg, "\n", "\r", -1)
 	derr := deeperror.New(num, msg, err)
-	log.Print(fmt.Sprintf("%v | [domain: %v][oid: %v][mid: %v][email: %v]",
+	log.Print(fmt.Sprintf("%v \r [domain: %v][oid: %v][mid: %v][email: %v]",
 		derr, domain, oid, mid, email,
 	))
 }
 
 func Plog(msg string, domain string, oid string, mid string, email string) {
 	msg = strings.Replace(msg, "\n", "\r", -1)
-	fmt.Println(fmt.Sprintf("Message: %v | [domain: %v][oid: %v][mid: %v][email: %v]",
+	fmt.Println(fmt.Sprintf("Message: %v \r [domain: %v][oid: %v][mid: %v][email: %v]",
 		msg, domain, oid, mid, email,
 	))
 }
